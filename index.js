031e0fc77620f58d9b9253687a3293fdf9345d66
Muhammad Daniyal Tallat <daniyaltallat0@gmail.com>
2025-08-30 11:36:45 -0700

    prisma setup

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 import express from "express";
-
 const app = express();
-
-import pool from './database/database.js';
-
-async function startApp() {
-  try {
-    const result = await pool.query('SELECT NOW()');  // test query
-    console.log('✅ Database connected at:', result.rows[0].now);
-  } catch (err) {
-    console.error('❌ Error connecting to DB:', err);
-  }
-}
-
-startApp();
 
 
 app.listen(3000, () => {
